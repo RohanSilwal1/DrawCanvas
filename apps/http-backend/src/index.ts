efeabb0593cf29f081052808fdf2d07c3b52a109
Rohan Silwal <rohansilwal10@gmail.com>
2026-02-25 23:09:43 +0545

    added prisma and defined schema for it

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,7 +1,23 @@
 import express from "express";
-
+import jwt from "jsonwebtoken"
 const app=express();
 
+app.post("/signup",(req,res)=>{
 
+    const username = req.body.username;
+    const password = req.body.password;
+
+})
+
+app.post("/signin",(req,res)=>{
+
+
+    const token=jwt
+})
+
+app.post("/create-room",(req,res)=>{
+
+    
+})
 
 app.listen(3005);
