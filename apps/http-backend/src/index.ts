462e749c8e3069d97e3bc4d35b15b34276af11b3
Rohan Silwal <rohansilwal10@gmail.com>
2026-02-28 18:35:39 +0545

    added signup endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -4,17 +4,41 @@ import { z } from "zod";
 import { prisma } from "@repo/db/client";
 import { CreateRoomSchema, CreateUserSchema, SigninSchema } from "@repo/common/types";
 const app = express();
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        return res.status(411).json({
+            message: "Incorrect Inputs"
+        })
+    }
+    try {
+        console.log(parsedData.data.email);
+        console.log(parsedData.data.password);
+        console.log(parsedData.data.name);
+
+        const user = await prisma.user.create({
+            data: {
+                email: parsedData.data.email,
+                password: parsedData.data.password,
+                name: parsedData.data.name
+            }
+        })
+        res.json({
+            userId: user.id
+        })
+    } catch (error: any) {
+        console.log(error.message);
+        return res.status(411).json({
+            message: "user already exist with same email"
+        })
+    }
 
 })
 
 app.post("/signin", (req, res) => {
 
-
-    const token = jwt
 })
 
 app.post("/create-room", (req, res) => {
