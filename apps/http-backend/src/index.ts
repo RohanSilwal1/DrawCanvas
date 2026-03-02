b60482013cc87a3ba25b57355dcb97eec0748a9a
Rohan Silwal <rohansilwal10@gmail.com>
2026-03-02 22:02:18 +0545

    added create-room http endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 import { prisma } from "@repo/db/client";
 import { CreateRoomSchema, CreateUserSchema, SigninSchema } from "@repo/common/types";
 import dotenv from "dotenv"
+import { middleware } from "./middleware";
 const app = express();
 app.use(express.json());
 
@@ -62,9 +63,28 @@ app.post("/signin", async (req, res) => {
     })
 })
 
-app.post("/create-room", (req, res) => {
+app.post("/create-room", middleware, async (req, res) => {
 
+    const parsedData = CreateRoomSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        return res.status(411).json({
+            message: "Incorrect Inputs"
+        })
+    }
+    const userId = req.userId;
+    if (!userId) {
+        throw new Error("User not authenticated");
+    }
 
+    await prisma.room.create({
+        data: {
+            slug: parsedData.data.message,
+            adminId: userId
+        }
+    })
+    res.status(200).json({
+        roomId:1234
+    })
 })
 
 app.listen(3005);
