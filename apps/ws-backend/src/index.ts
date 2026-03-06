433b074e1eeabcc7f13f8956fef4ab4bca10baeb
Rohan Silwal <rohansilwal10@gmail.com>
2026-03-06 21:10:02 +0545

    added authentication

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,10 +1,34 @@
 import { WebSocketServer } from "ws";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv"
+const wss = new WebSocketServer({ port: 8080 })
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET as string;
 
+function checkUser(token: string): string |null {
+    const decoded = jwt.verify(token, JWT_SECRET)
 
-const wss =new WebSocketServer({port:8080})
+    if (typeof decoded == "string") {
+        return null
+    }
+    if (!decoded || !decoded.userId) {
+        return null
+    }
+    return decoded.userId;
+}
 
-wss.on('connection',function connection(ws){
-    ws.on("message",function message(data){
+wss.on('connection', function connection(ws, request) {
+    const url = request.url;
+    if (!url) {
+        return;
+    }
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token') || "";
+    const userAuthenticated=checkUser(token)
+    if(!userAuthenticated){
+        ws.close();
+    }
+    ws.on("message", function message(data) {
         ws.send("pong");
     })
 })
