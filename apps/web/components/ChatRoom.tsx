526f56bfd765183140dc3be1a09a32ccd2fa1260
Rohan Silwal <rohansilwal10@gmail.com>
2026-03-11 23:58:56 +0545

    fixed typo

--- apps/web/components/ChatRoom.tsx
+++ apps/web/components/ChatRoom.tsx
@@ -1,12 +1,19 @@
 import axios from "axios";
 import dotenv from "dotenv";
+import ChatRoomClient from "./ChatRoomClient";
+
 async function GetChats(roomId: string) {
   dotenv.config();
   const BACKEND_URL = process.env.BACKEND_URL;
-  const response = await axios.get(`${BACKEND_URL}/room/${roomId}`);
+  console.log(`response message : ${roomId}`);
+
+  const response = await axios.get(`${BACKEND_URL}/chats/${roomId}`);
+  console.log(`response message : ${response.data.message}`);
+
   return response.data.message;
 }
 
 export default async function ChatRoom({ id }: { id: string }) {
-  const messages = GetChats(id);
+  const messages = await GetChats(id);
+  return <ChatRoomClient id={id} messages={messages} />;
 }
