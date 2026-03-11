b4580ee94d9f4c44cb96b5482c1ff9265e4ee094
Rohan Silwal <rohansilwal10@gmail.com>
2026-03-12 00:02:06 +0545

    fixed websocket url error

--- apps/web/hooks/useSocket.ts
+++ apps/web/hooks/useSocket.ts
@@ -1,19 +1,20 @@
 import dotenv from "dotenv";
 import { useEffect, useState } from "react";
 
-export default function useSocket(){
-    dotenv.config();
-    const [loading,setLoading]=useState(true);
-    const [socket,setSocket]=useState<WebSocket>();
-    const WS_URL=process.env.WS_URL as string;
-    useEffect(()=>{
-        const ws =new WebSocket(WS_URL);
-        ws.onopen=()=>{
+export function useSocket() {
+    const WS_URL = process.env.NEXT_PUBLIC_WS_URL as string;
+    console.log(`WS_URL ${WS_URL}`);
+
+    const [loading, setLoading] = useState(true);
+    const [socket, setSocket] = useState<WebSocket>();
+    useEffect(() => {
+        const ws = new WebSocket(`${WS_URL}? remaning to add jwt token here `);
+        ws.onopen = () => {
             setLoading(false);
             setSocket(ws);
         }
-    })
+    }, []);
     return {
-        socket,loading 
+        socket, loading
     }
 }
