7ebf6dad2b5674414dded4442c61f915d3eaab15
Rohan Silwal <rohansilwal10@gmail.com>
2026-03-11 23:58:19 +0545

    added dynamic route

--- apps/web/app/room/[slug]/page.tsx
+++ apps/web/app/room/[slug]/page.tsx
@@ -1,16 +1,20 @@
 import axios from "axios";
 import dotenv from "dotenv";
+import ChatRoom from "../../../components/ChatRoom";
 
 async function getRoomId(slug: string) {
   dotenv.config();
   const BACKEND_URL = process.env.BACKEND_URL;
   const response = await axios.get(`${BACKEND_URL}/room/${slug}`);
-  return response.data.id;
+  return response.data.room.id;
 }
 
-export default async function ChatRoom({ params }: { params: { slug: string } }) {
-  const slug = params.slug;
-  const roomId=await getRoomId(slug);
-  return;
-  <div>hello</div>;
+export default async function ChatRoom1({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const slug = (await params).slug;
+  const roomId = await getRoomId(slug);
+  return <ChatRoom id={roomId} />;
 }
